9eac3ed3eea36253c4edd67941e795e69145f50f
masfreddy93 <masieronicola@gmail.com>
2022-07-25 15:59:19 +0200

    ex done

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,1 +1,25 @@
-console.log('ciao')
+// chiedere all'utente la mail
+
+const mailUtente = prompt('Digitare email per favore');
+// console.log(mailUtente);
+
+// creare un array contenente una serie di mail
+const listaMail = ['mariorossi@yahoo.com', 'giovannimuciacia@gmail.com', 'ginorosario@blu.it', 'erbellino@bella.com', 'fedelarue@gmail.com'];
+// console.log(listaMail)
+let ePresente = false;
+
+// verificare se la mail digitata è contenuta nell'array
+for(let i=0; i < listaMail.length; i++){
+    let seNellaLista = listaMail[i];
+    // console.log(seNellaLista); 
+
+    if(mailUtente === seNellaLista){
+        ePresente = true;
+    }
+}
+
+if(ePresente === true){
+    console.log('La mail ' + mailUtente + ' è presente nell\'elenco');
+} else {
+    console.log('La mail ' +mailUtente + ' non è presente nell\'elenco');
+}
