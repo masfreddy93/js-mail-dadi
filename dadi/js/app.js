396b5593f11966216db0dc41b00ca97e8f800765
masfreddy93 <masieronicola@gmail.com>
2022-07-25 16:19:07 +0200

    ex done / commit pre ticket

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -1,1 +1,16 @@
-console.log('ciao')
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// C’è forse un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+
+let numeroRandomGiocatore = Math.floor((Math.random() * 6)+1);
+let numeroRandomComputer = Math.floor((Math.random() * 6)+1);
+console.log(numeroRandomGiocatore, numeroRandomComputer);
+if(numeroRandomGiocatore>numeroRandomComputer) {
+    console.log('Ha vinto il giocatore');
+} else if (numeroRandomGiocatore<numeroRandomComputer){
+    console.log('Ha vinto il computer');
+} else {
+    console.log('Pareggio')
+}
